c1e96f7dbdf07b2a902b952c09ce38c5c3814cf2
Jeremy King <codazendev@gmail.com>
2025-10-15 23:24:43 -0400

    added starting Logger class specification to header file

--- include/traverse/Logger.hpp
+++ include/traverse/Logger.hpp
@@ -5,10 +5,89 @@
 #pragma once
 
 // System includes.
+#include <format>
+#include <fstream>
 
 // Engine includes.
 
 // Declare this as part of the traverse namespace.
 namespace traverse {
+    // Logger class specification.
+    class Logger {
+        public:
+            // Enum class for supported log levels.
+            enum class Level {
+                Debug,
+                Info,
+                Warn,
+                Error,
+                Fatal
+            };
+        
+        private:
+            // The output file stream for the log file.
+            std::ofstream m_file_stream;
 
+            // Write log message to log file.
+            void log(Level level, const std::string& message);
+
+            static const char* levelToString(Level level);
+
+        public:
+            // Construct new Logger instance.
+            explicit Logger(const std::string& a_file_name);
+
+            // Destruct Logger instance.
+            ~Logger();
+
+            // Write debug log message without formatting.
+            void debug(const std::string& message);
+
+            // Write debug log message with formatting.
+            template<typename... Args>
+            void debugf(std::string_view fmt, Args&&... args) {
+                std::string formatted_message = std::vformat(fmt, std::make_format_args(args...));
+                log(Level::Debug, formatted_message);
+            }
+
+            // Write info log message without formatting.
+            void info(const std::string& message);
+
+            // Write info log message with formatting.
+            template<typename... Args>
+            void infof(std::string_view fmt, Args&&... args) {
+                std::string formatted_message = std::vformat(fmt, std::make_format_args(args...));
+                log(Level::Info, formatted_message);
+            }
+
+            // Write warn log message without formatting.
+            void warn(const std::string& message);
+
+            // Write warn log message with formatting.
+            template<typename... Args>
+            void warnf(std::string_view fmt, Args&&... args) {
+                std::string formatted_message = std::vformat(fmt, std::make_format_args(args...));
+                log(Level::Warn, formatted_message);
+            }
+
+            // Write error log message without formatting.
+            void error(const std::string& message);
+
+            // Write error log message with formatting.
+            template<typename... Args>
+            void errorf(std::string_view fmt, Args&&... args) {
+                std::string formatted_message = std::vformat(fmt, std::make_format_args(args...));
+                log(Level::Error, formatted_message);
+            }
+
+            // Write fatal log message without formatting.
+            void fatal(const std::string& message);
+
+            // Write fatal log message with formatting.
+            template<typename... Args>
+            void fatalf(std::string_view fmt, Args&&... args) {
+                std::string formatted_message = std::vformat(fmt, std::make_format_args(args...));
+                log(Level::Fatal, formatted_message);
+            }
+    };
 } // End traverse namespace.
