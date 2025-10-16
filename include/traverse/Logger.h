34b98da1663f87f8f2fa99126cd5d2a5ba5d15e5
Jeremy King <codazendev@gmail.com>
2025-10-15 21:50:44 -0400

    renamed Logger.h to Logger.hpp

--- include/traverse/Logger.h
+++
@@ -1,2 +0,0 @@
-// File: Logger.h
-// The engine logger.
---
+++ include/traverse/Logger.hpp
@@ -0,0 +1,14 @@
+// File: Logger.hpp
+// The engine logger.
+
+// Only include this file once during compilation.
+#pragma once
+
+// System includes.
+
+// Engine includes.
+
+// Declare this as part of the traverse namespace.
+namespace traverse {
+
+} // End traverse namespace.
