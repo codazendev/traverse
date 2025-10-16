4923358e41615788a05b21b5579a8b934922704f
Jeremy King <codazendev@gmail.com>
2025-10-16 01:23:41 -0400

    added basic Catch2 test

---
+++ tests/Logger.test.cpp
@@ -0,0 +1,13 @@
+// File: Logger.test.cpp
+// Tests for the Logger class.
+
+// System includes.
+
+// Engine includes.
+
+// Catch2 includes.
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Catch2 is working", "[testing]") {
+    REQUIRE(1 == 1);
+}
