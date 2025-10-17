c98a831153ef414e912645b549e049a6d8fd2bf5
Jeremy King <codazendev@gmail.com>
2025-10-16 20:32:55 -0400

    added file existence test to Logger test file

--- tests/Logger.test.cpp
+++ tests/Logger.test.cpp
@@ -2,12 +2,23 @@
 // Tests for the Logger class.
 
 // System includes.
+#include <string>
+#include <filesystem>
 
 // Engine includes.
+#include "traverse/Logger.hpp"
 
 // Catch2 includes.
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Catch2 is working", "[testing]") {
-    REQUIRE(1 == 1);
+TEST_CASE("traverse::Logger::levelToString", "[Logger]") {
+    std::string log_file_name = "log_test_" + std::to_string(random()) + ".log";
+    
+    REQUIRE_FALSE(std::filesystem::exists(log_file_name));
+
+    traverse::Logger logger(log_file_name);
+    
+    REQUIRE(std::filesystem::exists(log_file_name));
+    
+    logger.debug("Test debug message");
 }
